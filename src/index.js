8c891af2ce3b7844818a513ee0222ee9d2187bdb
lijiangtao <jiangtaoli1126@163.com>
2020-10-10 11:09:54 +0800

    修改Vue.use不成功的bug

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import dragModalVue from './components/modal.vue'
 
-export default dragModalVue
+// 在使用组件时，我们提供了Vue.use()的方式，如果使用Vue.use
+// 就会调用本身的 install 方法，同时传一个 Vue 这个类的参数，所以一定要像下面这样写
+
+const DragModalVueComponent = {
+    install:function(Vue){
+        Vue.component('dragModalVue',dragModalVue)
+    }
+}
+
+export default DragModalVueComponent
